22bfae3dddb8f7f964f9a685ba1f408540a1a121
iMaeGoo <mail1st@qq.com>
2023-11-28 15:47:28 +0800

    Update fixname.py

--- fixname.py
+++ fixname.py
@@ -27,20 +27,20 @@ def remove_emoji(string, replace_with="emoji"):
         "\U0001F300-\U0001F5FF"  # symbols & pictographs
         "\U0001F680-\U0001F6FF"  # transport & map symbols
         "\U0001F1E0-\U0001F1FF"  # flags (iOS)
-        "\U00002500-\U00002BEF"  # chinese char
-        "\U00002702-\U000027B0"
-        "\U00002702-\U000027B0"
-        "\U000024C2-\U0001F251"
-        "\U0001f926-\U0001f937"
-        "\U00010000-\U0010ffff"
-        "\u2640-\u2642"
-        "\u2600-\u2B55"
-        "\u200d"
-        "\u23cf"
-        "\u23e9"
-        "\u231a"
-        "\ufe0f"  # dingbats
-        "\u3030"
+        # "\U00002500-\U00002BEF"  # chinese char
+        # "\U00002702-\U000027B0"
+        # "\U00002702-\U000027B0"
+        # "\U000024C2-\U0001F251"
+        # "\U0001f926-\U0001f937"
+        # "\U00010000-\U0010ffff"
+        # "\u2640-\u2642"
+        # "\u2600-\u2B55"
+        # "\u200d"
+        # "\u23cf"
+        # "\u23e9"
+        # "\u231a"
+        # "\ufe0f"  # dingbats
+        # "\u3030"
         "]+",
         flags=re.UNICODE,
     )
@@ -89,7 +89,7 @@ if __name__ == "__main__":
         required=False,
     )
     args = vars(parser.parse_args())
-    replace_with = args["replace"] or "(emoji)"
+    replace_with = args["replace"] or ""
     if args["dir"]:
         dirname = args["dir"]
         if os.path.isdir(dirname):
